ac66de8f8b05c11b0fe51b6cdffeb54c49d42b08
vinodh241 <a.vinodh9072@gmail.com>
2025-12-01 13:18:38 +0530

    roboshop-automation-using shell scripting

--- dispatch.sh
+++ dispatch.sh
@@ -26,3 +26,49 @@ fi
                 echo -e " $2 is .... $R FAILED $N" | tee -a $LOG_FILE
             fi
         }  
+dnf install golang -y &>> $LOG_FILE
+validate $? "Golang installation"
+
+id roboshop &>> $LOG_FILE
+if [ $? -ne 0 ]
+then    
+    useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop    &>>$LOG_FILE
+    validate $? "Creating roboshop system user"
+else
+    echo -e "System user roboshop already created ... $Y SKIPPING $N"
+fi  
+mkdir -p /app
+validate $? "app directory creation"
+curl -L -o /tmp/dispatch.zip https://roboshop-artifacts.s3.amazonaws.com/dispatch-v3.zip 
+
+rm -rf /app/* 
+cd /app
+unzip /tmp/dispatch.zip  | tee -a $LOG_FILE
+validate $? "dispatch code download and extract"
+
+cd /app
+go mod init dispatch &>> $LOG_FILE
+validate $? "dispatch module init"  
+go get &>> $LOG_FILE
+validate $? "dispatch module get"  
+go build &>> $LOG_FILE
+validate $? "dispatch build"    
+
+cp $SCRIPT_DIR/dispatch.service /etc/systemd/system/dispatch.service &>> $LOG_FILE
+validate $? "dispatch systemd service file copy"  
+
+systemctl daemon-reload  | tee -a $LOG_FILE 
+validate $? "systemd daemon reload" 
+
+
+systemctl enable dispatch  | tee -a $LOG_FILE
+validate $? "dispatch enable service"
+systemctl start dispatch    | tee -a $LOG_FILE      
+validate $? "dispatch start service"    
+
+echo -e "script ended exuction time : $(date)" | tee -a $LOG_FILE
+echo -e "$G INFO :: dispatch setup completed successfully $N" | tee -a $LOG_FILE
+
+
+
+
