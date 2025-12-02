07cddb419a298897663334b57c52e2f1f8af0dfa
vinodh241 <a.vinodh9072@gmail.com>
2025-12-02 15:00:09 +0530

    roboshop-automation-using shell scripting

--- redis.sh
+++ redis.sh
@@ -1,57 +1,114 @@
+# #!/bin/bash
+
+# USERID=$(id -u)
+# R="\e[31m"
+# G="\e[32m"
+# N="\e[0m"   
+
+# LOG_FOLDER="/var/log/shell-roboshop.logs"
+# SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+# LOG_FILE="$LOG_FOLDER/$SCRIPT_NAME.log"
+# mkdir -p $LOG_FOLDER
+# SCRIPT_DIR=$PWD
+# echo -e "script started exuction time : $(date)" | tee -a $LOG_FILE
+
+# # check the user has root priveleges or not
+
+# if [ $USERID -ne 0 ]
+# then
+#     echo -e "$R ERROR:: pleae run this script with root access $N" | tee -a $LOG_FILE
+#     exit 1 # we can give other than zero upto 127
+# else
+#     echo -e "$G Success:: you are running with root access $N" | tee -a $LOG_FILE
+# fi
+
+# # validate functions takes input as exit status, what command they tried to install
+
+#         validate () {
+#             if [ $1 -eq 0 ]
+#             then
+#                 echo -e " $2 is .... $G SUCCESS $N" | tee -a $LOG_FILE
+#             else 
+#                 echo -e " $2 is .... $R FAILED $N" | tee -a $LOG_FILE
+#             fi
+#         }  
+
+# dnf module disable redis -y    | tee -a $LOG_FILE
+# validate $? "Redis module disable"
+
+# dnf module enable redis:7 -y  | tee -a $LOG_FILE
+# validate $? "Redis module enable"
+
+# dnf install redis -y  &>>$LOG_FILE
+# validate $? "Redis installation"
+
+
+# sed -i -e 's/127.0.0.1/0.0.0.0/g' -e '/protected-mode/ c protected-mode no' /etc/redis/redis.conf
+# validate $? "Edited redis.conf to accept remote connections"
+
+# systemctl enable redis  | tee -a $LOG_FILE
+# validate $? "Redis enable service"  
+# systemctl start redis  | tee -a $LOG_FILE
+# validate $? "Redis start service"       
+
+# echo -e "script ended exuction time : $(date)" | tee -a $LOG_FILE
+# echo -e "$G INFO :: Redis setup completed successfully $N" | tee -a $LOG_FILE
+
+
 #!/bin/bash
 
+START_TIME=$(date +%s)
 USERID=$(id -u)
 R="\e[31m"
 G="\e[32m"
-N="\e[0m"   
-
-LOG_FOLDER="/var/log/shell-roboshop.logs"
+Y="\e[33m"
+N="\e[0m"
+LOGS_FOLDER="/var/log/roboshop-logs"
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-LOG_FILE="$LOG_FOLDER/$SCRIPT_NAME.log"
-mkdir -p $LOG_FOLDER
-SCRIPT_DIR=$PWD
-echo -e "script started exuction time : $(date)" | tee -a $LOG_FILE
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+
+mkdir -p $LOGS_FOLDER
+echo "Script started executing at: $(date)" | tee -a $LOG_FILE
 
 # check the user has root priveleges or not
-
 if [ $USERID -ne 0 ]
 then
-    echo -e "$R ERROR:: pleae run this script with root access $N" | tee -a $LOG_FILE
-    exit 1 # we can give other than zero upto 127
+    echo -e "$R ERROR:: Please run this script with root access $N" | tee -a $LOG_FILE
+    exit 1 #give other than 0 upto 127
 else
-    echo -e "$G Success:: you are running with root access $N" | tee -a $LOG_FILE
+    echo "You are running with root access" | tee -a $LOG_FILE
 fi
 
 # validate functions takes input as exit status, what command they tried to install
+VALIDATE(){
+    if [ $1 -eq 0 ]
+    then
+        echo -e "$2 is ... $G SUCCESS $N" | tee -a $LOG_FILE
+    else
+        echo -e "$2 is ... $R FAILURE $N" | tee -a $LOG_FILE
+        exit 1
+    fi
+}
 
-        validate () {
-            if [ $1 -eq 0 ]
-            then
-                echo -e " $2 is .... $G SUCCESS $N" | tee -a $LOG_FILE
-            else 
-                echo -e " $2 is .... $R FAILED $N" | tee -a $LOG_FILE
-            fi
-        }  
+dnf module disable redis -y &>>$LOG_FILE
+VALIDATE $? "Disabling Default Redis version"
 
-dnf module disable redis -y    | tee -a $LOG_FILE
-validate $? "Redis module disable"
+dnf module enable redis:7 -y &>>$LOG_FILE
+VALIDATE $? "Enabling Redis:7"
 
-dnf module enable redis:7 -y  | tee -a $LOG_FILE
-validate $? "Redis module enable"
-
-dnf install redis -y  &>>$LOG_FILE
-validate $? "Redis installation"
-
+dnf install redis -y &>>$LOG_FILE
+VALIDATE $? "Installing Redis"
 
 sed -i -e 's/127.0.0.1/0.0.0.0/g' -e '/protected-mode/ c protected-mode no' /etc/redis/redis.conf
-validate $? "Edited redis.conf to accept remote connections"
+VALIDATE $? "Edited redis.conf to accept remote connections"
 
-systemctl enable redis  | tee -a $LOG_FILE
-validate $? "Redis enable service"  
-systemctl start redis  | tee -a $LOG_FILE
-validate $? "Redis start service"       
+systemctl enable redis &>>$LOG_FILE
+VALIDATE $? "Enabling Redis"
 
-echo -e "script ended exuction time : $(date)" | tee -a $LOG_FILE
-echo -e "$G INFO :: Redis setup completed successfully $N" | tee -a $LOG_FILE
+systemctl start redis  &>>$LOG_FILE
+VALIDATE $? "Started Redis"
 
+END_TIME=$(date +%s)
+TOTAL_TIME=$(( $END_TIME - $START_TIME ))
 
+echo -e "Script exection completed successfully, $Y time taken: $TOTAL_TIME seconds $N" | tee -a $LOG_FILE
